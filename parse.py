bb131628569cbebdd8450181a346fdc6be1c56d0
Siyuan Zhou <visualzhou@gmail.com>
2019-03-10 16:57:59 -0400

    Add HTML.

--- parse.py
+++ parse.py
@@ -1,46 +1,12 @@
 from lark import Lark, Transformer, v_args
-
-text = 'log = (N1 :> <<>> @@ N2 :> <<>> @@ N3 :> <<>> @@ N4 :> <<>> @@ N5 :> <<>>)'
-
-text2 = r"""
-state = ( N1 :> Leader @@
-         N2 :> Follower @@
-         N3 :> Follower @@
-         N4 :> Follower @@
-         N5 :> Follower )
-"""
-
-text3 = r"""
-/\ globalCurrentTerm = 1
-/\ log = (N1 :> <<>> @@ N2 :> <<>> @@ N3 :> <<>> @@ N4 :> <<>> @@ N5 :> <<>>)
-"""
-
-text4 = r"""
-log = (N1 :> <<>> @@ N2 :> <<>> @@ N3 :> <<>> @@ N4 :> <<>> @@ N5 :> <<>>)
-"""
-
-text5 = r"""
-/\ globalCurrentTerm = 0
-/\ log = (N1 :> <<>> @@ N2 :> <<>> @@ N3 :> <<>> @@ N4 :> <<>> @@ N5 :> <<>>)
-/\ state = ( N1 :> Follower @@
-  N2 :> Follower @@
-  N3 :> Follower @@
-  N4 :> Follower @@
-  N5 :> Follower )
-/\ commitPoint = ( N1 :> [index |-> 0, term |-> 0] @@
-  N2 :> [index |-> 0, term |-> 0] @@
-  N3 :> [index |-> 0, term |-> 0] @@
-  N4 :> [index |-> 0, term |-> 0] @@
-  N5 :> [index |-> 0, term |-> 0] )
-"""
-
-text6 = r"""
-[index |-> 0, term |-> 0]
-"""
+import jinja2
 
 class TreeToString(Transformer):
     def __init__(self, text):
         self.text = text
+
+    def get_str(metd):
+        return self.text[meta.start_pos:meta.end_pos] if not meta.empty else ""
 
     @v_args(meta=True)
     def show_text(self, children, meta):
@@ -48,22 +14,44 @@ class TreeToString(Transformer):
 
     # Stop at "value" rule and return the string under it.
     value = show_text
+    state_name = show_text
 
-    def delegate_to_children(self, children):
-        return children
+    def state(self, children):
+        print(children[0])
+        return (children[0], children[1:])
 
-    states = delegate_to_children
-    statement = delegate_to_children
+class State:
+    def __init__(self, name, variables):
+        self.name = name
+        self.variables = variables
 
 def parse_output(text):
     with open("output-grammar.lark", "r") as grammar:
-        parser = Lark(grammar, start='states', propagate_positions=True)
-        return parser.parse(text)
+        parser = Lark(grammar, start='behavior', propagate_positions=True)
+        tree = parser.parse(text)
+        print(tree.pretty())
+        result = TreeToString(text).transform(tree)
+        print(result)
+        return result
 
+def generate_html(states):
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(searchpath="./"),
+        autoescape=jinja2.select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template("template.html")
+    print(template.render(states=states, header=states[0].variables.keys()))
+
+
+def test_html():
+    state = State("State 1", {"log": "<here is log>", "term": "terms"})
+    generate_html([state, state])
 
 def main():
-    tree = parse_output(text5)
-    print(tree.pretty())
-    print(TreeToString(text5).transform(tree))
+    # with open("tla.out", "r") as tla_output:
+    #     text = tla_output.read()
+    #     behavior = parse_output(text)
+    test_html()
 
-main()
+if __name__ == "__main__":
+    main()
