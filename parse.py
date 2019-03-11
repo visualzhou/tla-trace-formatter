675799ca3fd4ab51db8f5a9541d7a876e60e27b7
Siyuan Zhou <visualzhou@gmail.com>
2019-03-10 22:45:38 -0400

    Generate HTML.

--- parse.py
+++ parse.py
@@ -1,46 +1,60 @@
 from lark import Lark, Transformer, v_args
 import jinja2
-
-class TreeToString(Transformer):
-    def __init__(self, text):
-        self.text = text
-
-    def get_str(metd):
-        return self.text[meta.start_pos:meta.end_pos] if not meta.empty else ""
-
-    @v_args(meta=True)
-    def show_text(self, children, meta):
-        return self.text[meta.start_pos:meta.end_pos] if not meta.empty else ""
-
-    # Stop at "value" rule and return the string under it.
-    value = show_text
-    state_name = show_text
-
-    def state(self, children):
-        print(children[0])
-        return (children[0], children[1:])
+import html
 
 class State:
     def __init__(self, name, variables):
         self.name = name
         self.variables = variables
 
+    def __str__(self):
+        return "{0}\n{1}".format(
+            self.name,
+            " ".join("{0} = {1}".format(k, v) for k, v in self.variables.items()))
+
+    def __repr__(self):
+        return self.__str__()
+
+class TreeToStates(Transformer):
+    def __init__(self, text):
+        self.text = text
+
+    def get_str(self, meta):
+        text = self.text[meta.start_pos:meta.end_pos] if not meta.empty else ""
+        return html.escape(text).replace("\n", "<br />")
+
+    @v_args(meta=True)
+    def show_text(self, children, meta):
+        return self.get_str(meta)
+
+    # Stop at "value" rule and return the string under it.
+    value = show_text
+    state_name = show_text
+    # behavior is a list of states.
+    behavior = list
+
+    def state(self, children):
+        name = children[0]
+        variables = {v.children[0]: v.children[1] for v in children[1:]}
+        return State(name, variables)
+
 def parse_output(text):
     with open("output-grammar.lark", "r") as grammar:
         parser = Lark(grammar, start='behavior', propagate_positions=True)
         tree = parser.parse(text)
-        print(tree.pretty())
-        result = TreeToString(text).transform(tree)
-        print(result)
+        # print(tree.pretty())
+        result = TreeToStates(text).transform(tree)
+        # print(result)
         return result
 
 def generate_html(states):
     env = jinja2.Environment(
         loader=jinja2.FileSystemLoader(searchpath="./"),
-        autoescape=jinja2.select_autoescape(['html', 'xml'])
+        # autoescape=jinja2.select_autoescape(['html', 'xml'])
     )
     template = env.get_template("template.html")
-    print(template.render(states=states, header=states[0].variables.keys()))
+    generated = template.render(states=states, header=states[0].variables.keys())
+    print(generated)
 
 
 def test_html():
@@ -48,10 +62,11 @@ def test_html():
     generate_html([state, state])
 
 def main():
-    # with open("tla.out", "r") as tla_output:
-    #     text = tla_output.read()
-    #     behavior = parse_output(text)
-    test_html()
+    with open("tla.out", "r") as tla_output:
+        text = tla_output.read()
+        behavior = parse_output(text)
+        generate_html(behavior)
+
 
 if __name__ == "__main__":
     main()
